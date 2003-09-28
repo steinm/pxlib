9708efe8071b69e2b31799f6e891f7646d91f3b6
steinm <>
2003-09-28 11:33:51 +0000

    - added prototypes

--- src/px_head.h
+++ src/px_head.h
@@ -2,4 +2,6 @@
 #define __PX_HEAD_H__
 pxhead_t *get_px_head(pxdoc_t *pxdoc, FILE *fp);
 int put_px_head(pxdoc_t *pxdoc, pxhead_t *pxh, FILE *fp);
+int put_px_datablock(pxdoc_t *pxdoc, pxhead_t *pxh, int after, FILE *fp);
+int px_add_data_to_block(pxdoc_t *pxdoc, pxhead_t *pxh, int datablocknr, char *data, FILE *fp);
 #endif
