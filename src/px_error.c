d7fc4feb8f922cf94c325455ea4bf82778803cd1
steinm <>
2003-10-29 18:07:17 +0000

    - allow to pass user data to error handler

--- src/px_error.c
+++ src/px_error.c
@@ -4,10 +4,17 @@
 #include "px_intern.h"
 #include "paradox.h"
 
-void px_errorhandler(pxdoc_t *p, int error, const char *str) {
+/* px_errorhandler() {{{
+ * Default error handler if not set by application
+ */
+void px_errorhandler(pxdoc_t *p, int error, const char *str, void *data) {
 	printf("PXLib: %s\n", str);
 }
+/* }}} */
 
+/* px_error() {{{
+ * Issue an error from within the library by using the error handler
+ */
 void px_error(pxdoc_t *p, int type, const char *fmt, ...) {
 	char msg[256];
 	va_list ap;
@@ -18,7 +25,7 @@ void px_error(pxdoc_t *p, int type, const char *fmt, ...) {
 	if (!p->in_error) {
 		p->in_error = 1; /* avoid recursive errors */
 		if(p->errorhandler)
-			(p->errorhandler)(p, type, msg);
+			(p->errorhandler)(p, type, msg, p->errorhandler_user_data);
 	}
 
 	/* If the error handler returns the error was non-fatal */
@@ -26,3 +33,13 @@ void px_error(pxdoc_t *p, int type, const char *fmt, ...) {
 
 	va_end(ap);
 }
+/* }}} */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: sw=4 ts=4 fdm=marker
+ * vim<600: sw=4 ts=4
+ */
