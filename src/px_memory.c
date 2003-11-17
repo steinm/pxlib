ed7e73a8c23844cfaa0720cfcbb3100e9b99d942
steinm <>
2003-11-17 08:30:59 +0000

    - put '_' a prefix for intern mem management functions

--- src/px_memory.c
+++ src/px_memory.c
@@ -4,15 +4,15 @@
 #include "paradox.h"
 #include "px_error.h"
 
-void *px_malloc(pxdoc_t *p, size_t len, const char *caller) {
+void *_px_malloc(pxdoc_t *p, size_t len, const char *caller) {
 	return((void *) malloc(len));
 }
 
-void *px_realloc(pxdoc_t *p, void *mem, size_t len, const char *caller) {
+void *_px_realloc(pxdoc_t *p, void *mem, size_t len, const char *caller) {
 	return((void *) realloc(mem, len));
 }
 
-void px_free(pxdoc_t *p, void *ptr) {
+void _px_free(pxdoc_t *p, void *ptr) {
 	free(ptr);
 	ptr = NULL;
 }
